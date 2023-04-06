cd8ddda023f04e1847c66d1488470757bff1b3bb
juan lopez salamanca <juanlopezsalamanca1@gmail.com>
2023-04-05 20:33:34 -0500

    added notes and new varients of possible methods

---
+++ PalindromeExercise/PalindromeExercise/WordSmith.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace PalindromeExercise
+{
+    public class WordSmith
+    {
+        public bool IsAPalidrome(string word)
+        {
+            string newWord = word.ToLower();
+            
+            var reversedWord = string.Empty;
+            for (var i = newWord.Length - 1; i >= 0; i--)
+            {
+                reversedWord += newWord.ToLower()[i];
+            }
+            if (newWord == reversedWord)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+    }
+}
--- PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
+++ PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
@@ -1,3 +1,4 @@
+using PalindromeExercise;
 using System;
 using Xunit;
 
@@ -5,9 +6,22 @@ namespace PalindromeExerciseTests
 {
     public class UnitTest1
     {
-        [Fact]
-        public void Test1()
+        [Theory]
+        [InlineData("tacocat", true)]
+        [InlineData("Juan", false)]
+        [InlineData("Yelley", true)]
+        [InlineData("Neven", true)]
+        [InlineData("Kelly", false)]
+        [InlineData("Ava", true)]
+        [InlineData("Ciric", true)]
+        [InlineData("aa", true)]
+        public void IsAPalidromeTest(string word, bool expected)
         {
+            var tester = new WordSmith();
+
+            var actual = tester.IsAPalidrome(word);
+
+            Assert.Equal(expected, actual);
 
         }
     }
